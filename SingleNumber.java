01970a2068d5dc6d0a10811d8ab5327b49852bfa
Sandeep Karem <sandeepkarem.online@gmail.com>
2026-01-31 19:29:23 +0530

    leetcode #problem 136

--- SingleNumber.java
+++ SingleNumber.java
@@ -1,9 +1,9 @@
-import java.util.HashMap;
+
 // import java.utils.*;
 
 public class SingleNumber {
 
-    
+    // 136. Single number problem
     public static void main(String[] args) {
         int[] arr = {2,2,1};
         int res = 0;
