a1dd8fe35d4d2b2b5e56e3bcbfdfb37fb3477897
Sandeep Karem <sandeepkarem.online@gmail.com>
2026-02-01 19:17:28 +0530

    leetcode #problem 35

---
+++ SearchInsertPosition.java
@@ -0,0 +1,23 @@
+public class SearchInsertPosition {
+
+    public static int getpos(int arr[], int target){
+        int high = arr.length-1;
+        int low = 0;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(arr[mid]==target)return mid;
+            else if(target > arr[mid]){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return low;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1,3,5,6};
+        System.out.println(getpos(arr, 9));
+    }
+}
